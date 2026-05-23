6095f3c0b3d9915941d25c6d28873617a5d135eb
Admin <admin@Admins-MacBook-Air.local>
2026-05-23 10:04:38 +0100

    add homepage route

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.http import HttpResponse
+from django.urls import include, path
+
+
+def home(request):
+    return HttpResponse("OpsFlow is running successfully on Railway 🚀")
+
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("workflows/", include("workflows.urls")),
 ]
